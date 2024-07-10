66fca60c6c4b14e30d7f4d1fc6b247bf593b99b6
Luiz Villa <lflavado@laas.fr>
2024-07-10 10:11:49 +0200

    Updates the sogi example to object oriented programming

--- src/power_ac1phase.cpp
+++ src/power_ac1phase.cpp
@@ -1,27 +1,107 @@
+/*
+ *
+ * Copyright (c) 2021-2024 LAAS-CNRS
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 2.1 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: LGPL-2.1
+ */
+
+/**
+ * @brief  This file is a power control class for single phase inverters
+ *
+ * @author Regis Ruelland <rruelland@laas.fr>
+ * @author Luiz Villa <luiz.villa@laas.fr>
+ */
+
+
 #include "power_ac1phase.h"
 
-// TODO return error code
-void power_ac1phase_init(power_ac1phase_params_t *params, float32_t grid_voltage, float32_t w0, float32_t Ts) {
-    float32_t rise_time = 1.0F * 2.0F * PI / w0;
-    params->w0 = w0;
-    sogi_pll_init(&params->sogi_pll_params, grid_voltage, w0, rise_time, Ts);
-    float32_t Kr = 500;
-    sogi_init(&params->sogi_i_params, Kr, Ts);
+// Constructor for PowerAC1Phase
+PowerAC1Phase::PowerAC1Phase() : w0(0.0F) {}
+
+// Initialization function for PowerAC1Phase
+int8_t PowerAC1Phase::init(PowerAC1PhaseParams params) {
+    float32_t rise_time = 1.0F * 2.0F * PI / params.w0;
+    this->w0 = params.w0;
+
+    // Initialize SOGI-PLL for voltage
+    sogi_pll.init(params.grid_voltage, params.w0, rise_time, params.Ts);
+
+    // Initialize SOGI for current
+    sogi_i_params.Kr = 500;
+    sogi_i_params.Ts = params.Ts;
+    sogi_i_params.den[0] = sogi_i_params.den[1] = sogi_i_params.den[2] = 0.0F;
+    sogi_i_params.num[0] = sogi_i_params.num[1] = 0.0F;
+
+    return 0;  // Return 0 to indicate success
 }
 
-power_ac1phase_t power_ac1phase(float32_t v, float32_t i, power_ac1phase_params_t *params) {
-    dqo_t Idq;
-    clarke_t Iab;
-    dqo_t Vdq; 
-    power_ac1phase_t power;
-    sogi_pll_calc(v, &params->sogi_pll_params);
-    Iab = sogi_calc(i, params->w0, &params->sogi_i_params) ;
-    Idq = Transform::rotation_to_dqo(Iab, params->sogi_pll_params.theta);
-	params->Iab = Iab;
-	params->Idq = Idq;
-    Vdq = params->sogi_pll_params.Vdq;
-    power.p = 0.5F * (Vdq.d * Idq.d + Vdq.q * Idq.q);
-    power.q = 0.5F * (Idq.d * Vdq.q - Idq.q * Vdq.d);
+// Reset function for PowerAC1Phase
+void PowerAC1Phase::reset() {
+    sogi_i_params.den[0] = sogi_i_params.den[1] = sogi_i_params.den[2] = 0.0F;
+    sogi_i_params.num[0] = sogi_i_params.num[1] = 0.0F;
+}
+
+// Calculate function for PowerAC1Phase
+PowerAC1PhaseOutput PowerAC1Phase::calculate(float32_t v, float32_t i) {
+    PowerAC1PhaseOutput power;
+    // Perform SOGI-PLL calculation
+    sogi_pll.calculate(v);
+
+    // Perform SOGI calculation for current
+    clarke_t Iab = sogi_pll.sogi_calc(i, w0, sogi_i_params);
+
+    // Transform current from alpha-beta to d-q
+    dqo_t Idq = Transform::rotation_to_dqo(Iab, sogi_pll.params.theta);
+
+    // Save internal states
+    _Iab = Iab;
+    _Idq = Idq;
+    _Vdq = sogi_pll.params.Vdq;
+    _Vab = sogi_pll.params.Vab;
+    _w = sogi_pll.params.w;
+
+    // Calculate active and reactive power
+    power.p = 0.5F * (_Vdq.d * _Idq.d + _Vdq.q * _Idq.q);
+    power.q = 0.5F * (_Idq.d * _Vdq.q - _Idq.q * _Vdq.d);
+
     return power;
 }
 
+dqo_t PowerAC1Phase::getVdq(){
+    return _Vdq;
+}
+
+clarke_t PowerAC1Phase::getIab(){
+    return _Iab;
+}
+
+clarke_t PowerAC1Phase::getVab(){
+    return _Vab;
+}
+
+
+dqo_t PowerAC1Phase::getIdq(){
+    return _Idq;
+}
+
+float32_t PowerAC1Phase::getTheta(){
+    return sogi_pll.params.theta;
+}
+
+
+float32_t PowerAC1Phase::getw(){
+    return sogi_pll.params.w;
+}
--- src/sogi_pll.h
+++ src/sogi_pll.h
@@ -1,35 +1,91 @@
 #ifndef SOGI_PLL_H
 #define SOGI_PLL_H
+
 #include "pid.h"
-#ifndef __arm__ 
+#ifndef __arm__
 #include "my_types.h"
 #else
 #include "arm_math.h"
 #endif
 
 #include "transform.h"
-typedef struct {
-    float32_t num[2];
-    float32_t den[3];
-    float32_t Ts;
-    float32_t Kr;
-} sogi_params_t;
 
-typedef struct {
-    sogi_params_t sogi_params;
-	PidParams pi_params;
-    Pid pi;
-    float32_t Ts;
-    dqo_t Vdq;
-    clarke_t Vab;
-    float32_t theta;
-    float32_t next_theta;
-    float32_t w;
-    float32_t w_ref;
-} sogi_pll_params_t;
+/**
+ * @brief Structure containing parameters for the SOGI (Second-Order Generalized Integrator) filter.
+ */
+struct SogiParams {
+    float32_t num[2]; ///< Numerator coefficients of the SOGI filter.
+    float32_t den[3]; ///< Denominator coefficients of the SOGI filter.
+    float32_t Ts;     ///< Sampling time.
+    float32_t Kr;     ///< Resonance gain.
+};
 
-void sogi_init(sogi_params_t *params, float32_t Kr, float32_t Ts);
-clarke_t sogi_calc(float32_t input, float32_t w0, sogi_params_t *params);
-void sogi_pll_init(sogi_pll_params_t *sogi_pll_params, float32_t Vgrid, float32_t w0, float32_t rise_time, float32_t Ts);
-void sogi_pll_calc(float32_t v_grid, sogi_pll_params_t *sogi_pll_params);
-#endif
+/**
+ * @brief Structure containing parameters for the SOGI-PLL system.
+ */
+struct SogiPllParams {
+    SogiParams sogi_params; ///< SOGI parameters.
+    PidParams pi_params;    ///< PI controller parameters.
+    Pid pi;                 ///< PI controller.
+    float32_t Ts;           ///< Sampling time.
+    dqo_t Vdq;              ///< DQ axis voltage.
+    clarke_t Vab;           ///< Clarke transformation of voltage.
+    float32_t theta;        ///< Current phase angle.
+    float32_t next_theta;   ///< Next phase angle.
+    float32_t w;            ///< Angular frequency.
+    float32_t w_ref;        ///< Reference angular frequency.
+};
+
+/**
+ * @brief Class implementing the SOGI-PLL (Second-Order Generalized Integrator Phase-Locked Loop) system.
+ */
+class SogiPLL {
+public:
+    /**
+     * @brief Constructor for the SogiPLL class.
+     */
+    SogiPLL();
+
+    /**
+     * @brief Initializes the SOGI-PLL system.
+     *
+     * @param Vgrid Grid voltage.
+     * @param w0 Angular frequency.
+     * @param rise_time Desired rise time.
+     * @param Ts Sampling time.
+     */
+    void init(float32_t Vgrid, float32_t w0, float32_t rise_time, float32_t Ts);
+
+    /**
+     * @brief Performs the SOGI-PLL calculation.
+     *
+     * @param v_grid Grid voltage.
+     */
+    void calculate(float32_t v_grid);
+
+    SogiPllParams params; ///< Parameters for the SOGI-PLL system.
+
+    /**
+     * @brief Initializes the SOGI parameters.
+     *
+     * @param params Reference to the SOGI parameters structure.
+     * @param Kr Resonance gain.
+     * @param Ts Sampling time.
+     */
+    void sogi_init(SogiParams &params, float32_t Kr, float32_t Ts);
+
+    /**
+     * @brief Performs SOGI calculation.
+     *
+     * @param input Input signal.
+     * @param w0 Angular frequency.
+     * @param params Reference to the SOGI parameters structure.
+     * @return Clarke transformation result of the input signal.
+     */
+    clarke_t sogi_calc(float32_t input, float32_t w0, SogiParams &params);
+
+private:
+
+};
+
+#endif // SOGI_PLL_H
