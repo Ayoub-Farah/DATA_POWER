5f3b14a98ee5be2a2894ecbdb562f27fe0749add
Luiz Villa <luiz.villa@laas.fr>
2025-06-18 05:35:55 +0200

    Update to the can example with a master-slave communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-present LAAS-CNRS
+ * Copyright (c) 2021-present OwnTech Technologies
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU Lesser General Public License as published by
@@ -18,90 +18,167 @@
  */
 
 /**
- * @brief  This example shows how to blink the onboard LED of the Spin board.
+ * @brief  This example deploy a simple CAN based communication using
+ *         Thingset library.
  *
- * @author Clément Foucher <clement.foucher@laas.fr>
- * @author Luiz Villa <luiz.villa@laas.fr>
- * @author Ayoub Farah Hassan <ayoub.farah-hassan@laas.fr>
+ * @author Jean Alinei <jean.alinei@owntech.io>
  */
 
-/* --------------OWNTECH APIs---------------------------------- */
-#include "SpinAPI.h"
+/*--------------OWNTECH APIs---------------------------------- */
 #include "TaskAPI.h"
+#include "ShieldAPI.h"
+#include "SpinAPI.h"
+#include "user_data_objects.h"
+#include <thingset/can.h>
+#include "CommunicationAPI.h"
 
-/* --------------SETUP FUNCTIONS DECLARATION------------------- */
 
+/*--------------SETUP FUNCTIONS DECLARATION------------------- */
 /* Setups the hardware and software of the system */
 void setup_routine();
 
-/* --------------LOOP FUNCTIONS DECLARATION-------------------- */
+/*--------------LOOP FUNCTIONS DECLARATION-------------------- */
 
 /* Code to be executed in the background task */
 void loop_background_task();
 /* Code to be executed in real time in the critical task */
 void loop_critical_task();
 
-/* --------------USER VARIABLES DECLARATIONS------------------- */
+/*--------------USER VARIABLES DECLARATIONS------------------- */
+float32_t control_reference;
+float32_t received_value;
+bool      received_start_stop;
+bool master = false;
 
-
-
-/* --------------SETUP FUNCTIONS------------------------------- */
+/*--------------SETUP FUNCTIONS------------------------------- */
 
 /**
  * This is the setup routine.
- * It is used to call functions that will initialize your spin, power shields
- * and tasks.
+ * It is used to call functions that will initialize your hardware and tasks.
  *
- * In this example, we spawn a background task.
- * An optional critical task can be initialized by uncommenting the two
- * commented lines.
+ * In this default main, we only spawn two tasks
+ *  - A background task.
+ *  - A critical task is defined but not started.
+ *
+ * NOTE: It is important to follow the steps and initialize the hardware first
+ * and the tasks second.
  */
 void setup_routine()
 {
-    /* Declare task */
+    /* STEP 1 - SETUP THE HARDWARE */
+
+    /* Buck voltage mode */
+    shield.power.initBuck(ALL);
+    // shield.sensors.enableDefaultTwistSensors();
+    shield.sensors.enableDefaultOwnverterSensors();
+
+    /* Use functions below to enable or disable
+       - control over CAN
+       - measure broadcasting over CAN
+     */
+
+     spin.gpio.configurePin(20,OUTPUT);
+
+    /* STEP 2 - SETUP THE TASKS */
+    /* Control frames are not sent by default.*/
     uint32_t background_task_number =
                             task.createBackground(loop_background_task);
 
-    /* Uncomment following line if you use the critical task */
-    /* task.createCritical(loop_critical_task, 500); */
+    /* Uncomment the following line if you use the critical task */
+    task.createCritical(loop_critical_task, 100);
 
-    /* Finally, start tasks */
+    /* STEP 3 - LAUNCH THE TASKS */
     task.startBackground(background_task_number);
-    /* Uncomment following line if you use the critical task */
-    /* task.startCritical(); */
+
+    /* Uncomment the following line if you use the critical task */
+    task.startCritical();
 }
 
-/* --------------LOOP FUNCTIONS-------------------------------- */
+/*--------------LOOP FUNCTIONS-------------------------------- */
 
 /**
  * This is the code loop of the background task
- * It runs perpetually. Here a `suspendBackgroundMs` is used to pause during
- * 1000ms between each LED toggles.
- * Hence we expect the LED to blink each second.
+ * You can use it to execute slow code such as state-machines.
+ * The pause define its pseudo-periodicity.
  */
 void loop_background_task()
 {
-    /* Task content */
-    spin.led.toggle();
+    spin.gpio.resetPin(20);
+    communication.can.setCtrlEnable(true);
+    communication.can.setBroadcastEnable(true);
 
-    /* Pause between two runs of the task */
+    // shield.sensors.triggerTwistTempMeas(TEMP_SENSOR_1);
+
+    // meas_data = shield.sensors.getLatestValue(TEMP_SENSOR_1);
+    // if (meas_data != NO_VALUE) temp_1_value = meas_data;
+
+    // shield.sensors.triggerTwistTempMeas(TEMP_SENSOR_2);
+
+    // meas_data = shield.sensors.getLatestValue(TEMP_SENSOR_2);
+    // if (meas_data != NO_VALUE) temp_2_value = meas_data;
+
+    printk("%8.3f:", (double)I1_low_value);
+    printk("%8.3f:", (double)V1_low_value);
+    printk("%8.3f:", (double)I2_low_value);
+    printk("%8.3f:", (double)V2_low_value);
+    printk("%8.3f:", (double)I_high_value);
+    printk("%8.3f:", (double)V_high_value);
+    printk("%8.3f:", (double)temp_1_value);
+    printk("%8.3f:", (double)received_value);
+    printk("\n");
+
+    /* This pauses the task for 1000 milli seconds */
     task.suspendBackgroundMs(1000);
+    control_reference += 1.0;
+
+
+    if(master){
+        /* Use the following function to send a control reference over CAN */
+        communication.can.setCtrlReference(control_reference);
+        /* Use the following functions to send a start or stop order over CAN */
+        communication.can.startSlaveDevice();
+        // communication.can.stopSlaveDevice();
+        spin.led.toggle();
+
+    } else{
+        /* The following functions retrieve any reference sent over CAN */
+        received_value = communication.can.getCtrlReference();
+        /* The following functions retrieve any start stop order sent over CAN */
+        received_start_stop = communication.can.getStartStopState();
+        if(received_start_stop){
+            spin.led.turnOn();
+        }
+
+    }
 }
 
 /**
- * Uncomment lines in setup_routine() to use critical task.
- *
  * This is the code loop of the critical task
- * It is executed every 500 micro-seconds defined in the setup_software
- * function. You can use it to execute an ultra-fast code with
- * the highest priority which cannot be interrupted by the background tasks.
- *
- * In the critical task, you can implement your control algorithm that will
- * run in Real Time and control your power flow.
+ * It is executed every 100 micro-seconds defined in the setup_routine function.
+ * You can use it to execute an ultra-fast code with the highest priority which
+ * cannot be interrupted.
  */
 void loop_critical_task()
 {
+    if(master){
+        meas_data = shield.sensors.getLatestValue(I1_LOW);
+        if (meas_data != NO_VALUE) I1_low_value = meas_data;
 
+        meas_data = shield.sensors.getLatestValue(V1_LOW);
+        if (meas_data != NO_VALUE) V1_low_value = meas_data;
+
+        meas_data = shield.sensors.getLatestValue(V2_LOW);
+        if (meas_data != NO_VALUE) V2_low_value = meas_data;
+
+        meas_data = shield.sensors.getLatestValue(I2_LOW);
+        if (meas_data != NO_VALUE) I2_low_value = meas_data;
+
+        meas_data = shield.sensors.getLatestValue(I_HIGH);
+        if (meas_data != NO_VALUE) I_high_value = meas_data;
+
+        meas_data = shield.sensors.getLatestValue(V_HIGH);
+        if (meas_data != NO_VALUE) V_high_value = meas_data;
+    }
 }
 
 /**
